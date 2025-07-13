373aeb6f47d8dc34f85b08cfe5979c6a0b93a0d6
Lekhana-bhat-2004 <bhatlekhana2004@gmail.com>
2025-07-13 23:58:59 +0530

    added login page

---
+++ LOGIN REG/script.js
@@ -0,0 +1,15 @@
+
+const loginForm = document.getElementById("login-form");
+const registerForm = document.getElementById("register-form");
+const loginLink = document.getElementById("login-link");
+const registerLink = document.getElementById("register-link");
+
+loginLink.addEventListener("click", () => {
+    loginForm.style.display = "block";
+    registerForm.style.display = "none";
+});
+
+registerLink.addEventListener("click", () => {
+    loginForm.style.display = "none";
+    registerForm.style.display = "block";
+});
